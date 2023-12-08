9f22523cb0f01f40bc2eb391087e1b30f729af76
daeppang119 <dbsk9436@naver.com>
2023-12-08 10:03:08 +0900

    12/8

--- 약수의개수와덧셉.js
+++ 약수의개수와덧셉.js
@@ -7,7 +7,27 @@ function solution(left, right) {
     } else {
       answer += i;
     }
-    // 약수가 짝수이면.. 더하기 홀수이면.. 빼기
+  }
+  return answer;
+}
+
+function solution(left, right) {
+  // left - right까지의 모든 수에서 (약수의 개수가 짝수)? +: -;
+  var answer = 0;
+  for (let i = left; i <= right; i++) {
+    let arr = [];
+    // i는 13,14,15,16,17
+    // console.log(i)
+    for (let j = 1; j <= i; j++) {
+      // j는 123...13,123...14,,,123...17
+      // console.log(j)
+      //j가 i의 약수라면 배열에 담기
+      if (i % j == 0) {
+        arr.push(j);
+      }
+    }
+    console.log(arr);
+    arr.length % 2 === 0 ? (answer += i) : (answer -= i);
   }
   return answer;
 }
